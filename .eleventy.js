da58b7d54b75b00a1b184706d3c936fba7dc8969
Alan-Guro1 <90780535+Alan-Guro1@users.noreply.github.com>
2022-05-26 04:59:08 -0400

    done :100:

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+const { DateTime } = require("luxon");
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/style");
+  eleventyConfig.addPassthroughCopy("src/md");
+  eleventyConfig.addPassthroughCopy("src/pictures");
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
